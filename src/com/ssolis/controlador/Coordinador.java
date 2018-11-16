c24a3d43941da13bf225aacd2b8ebd9aa6b83b7d
PC-02 <PC-02@PC-03>
2018-11-16 10:02:09 -0500

    Programacion programa cordenadas circulo

---
+++ src/com/ssolis/controlador/Coordinador.java
@@ -0,0 +1,118 @@
+package com.ssolis.controlador;
+
+import com.ssolis.modelo.Circulo;
+import com.ssolis.modelo.Punto;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+  * @Instituto Rumiñahui
+ * @author SSolis
+ */
+public class Coordinador {
+    ArrayList<Punto> puntos= new ArrayList<Punto>();
+    ArrayList<Circulo> circulos= new ArrayList<Circulo>();
+    
+    public void almacenarPuntos(Punto p){
+        p.setX(Integer.parseInt(JOptionPane.showInputDialog("Ingresa X:")));
+         p.setY(Integer.parseInt(JOptionPane.showInputDialog("Ingresa Y:")));
+         puntos.add(p);
+    }
+    public void calcularDistanciaEntreDosPuntos(){
+        int p1,p2,cont=0;
+        //do{
+        p1 = Integer.parseInt(JOptionPane.showInputDialog(imprimirPuntos()+"\nIngresa un punto"))-1;
+        p2 = Integer.parseInt(JOptionPane.showInputDialog(imprimirPuntos()+"\nIngresa un punto"))-1;
+        double v1=Math.pow((puntos.get(p2).getX())-(puntos.get(p1).getX()),2);
+        double v2=Math.pow((puntos.get(p2).getY())-(puntos.get(p1).getY()),2);
+        JOptionPane.showMessageDialog(null,""+Math.sqrt(v1+v2));
+//        cont++;
+        //}while(cont!=2);
+//imprimirPuntos();
+    }
+    
+    public void crearCirculo(){
+      //  circulos.add(new Circulo(puntos.get(0),Integer.parseInt(JOptionPane.showInputDialog("Ingresa radio"))));
+    //JOptionPane.showMessageDialog(null,circulos.get(0));
+    
+    double pi = 3.14;
+        double radio = Double.parseDouble(JOptionPane.showInputDialog("ingrese el radio del circulo: "));
+        double puntos = 2*radio*pi;
+        double area = pi*Math.pow(radio,2);
+        JOptionPane.showMessageDialog(null,"Puntos: "+puntos+"\n"+"Área: "+area);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     public String imprimirPuntos(){
+        String datos="";
+        for (Punto punto : puntos) {
+            datos +="\n"+(puntos.indexOf(punto)+1)+".- "+punto.toString();
+        }
+        //JOptionPane.showMessageDialog(null, datos);
+        return datos;
+    }
+     
+     public void menu(){
+        int op=0;
+        
+        do{
+        try{
+            op = Integer.parseInt(JOptionPane.showInputDialog("Seleccione una opcion:\n"
+                 + "1.- Ingresar Punto\n"
+                 + "2.- Calcular Distancia entre dos puntos\n"
+               
+                 + "3.- Imprimir Puntos\n"
+                 + "4.- Crear circulo\n"  
+                 + "5.- Salir"));
+            switch (op) {
+                case 1:
+                    almacenarPuntos(new Punto());
+                    break;
+                case 2:
+                    calcularDistanciaEntreDosPuntos();
+                    System.out.println("CAso 2");
+                    break;
+                 
+                case 3:
+                    JOptionPane.showMessageDialog(null,imprimirPuntos());
+                    break;
+                case 4:
+                    crearCirculo();
+                    System.out.println("CAso 4");
+                    break;
+            } 
+        }catch(Exception e){
+            System.out.println("error");
+            
+        }
+           
+            
+        }while(op!=5);
+                 
+     }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/ssolis/vista/Aplicacion.java
@@ -0,0 +1,57 @@
+package com.ssolis.vista;
+
+import com.ssolis.controlador.Coordinador;
+import com.ssolis.modelo.Punto;
+import javax.swing.JOptionPane;
+
+/**
+  * @Instituto Rumiñahui
+ * @author SSolis
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        Coordinador coordinador= new Coordinador();
+        coordinador.menu();
+        
+        //double pi = 3.14;
+        //double radio = Double.parseDouble(JOptionPane.showInputDialog("ingrese el radio: "));
+        //double puntos = 2*radio*pi;
+        //double area = pi*Math.pow(radio,2);
+        //JOptionPane.showMessageDialog(null,"Puntos: "+puntos+"\n"+"area: "+area);
+        
+//        Punto p = new Punto();
+//        coordinador.almacenarPuntos(p);
+//        coordinador.imprimirPuntos();
+//        Punto p1 = new Punto(2, 3);
+//        Punto p2 = new Punto(5, 6);
+//        int op;
+//        do {         
+//          op=Integer.parseInt(JOptionPane.showInputDialog("Deseas ingresar un Punto..?\n"
+//                  + "1.- Si\n"
+//                  + "2.- No"));
+//         if(op==1)
+//          Punto.almacenarPuntos();
+//        } while (op != 2);
+//       
+//      Punto.imprimirPuntos();
+//        
+//        Punto.almacenarPuntos();
+//        JOptionPane.showMessageDialog(null, p1.toString()+"\n"+
+//                p2.toString()+"\n"+
+//                "La distancia entre los puntos son: "
+//                        + ""+Punto.calcularDistancia(p1, p2) );
+//        
+//        
+//        
+//        
+//        
+//        
+//        
+//        
+//        
+        
+        
+        
+    }
+
+}
